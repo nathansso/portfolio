b79d99807505d3e5cfe77c182ba780788bbfc366
nathansso <noliver@ucsd.edu>
2025-02-06 17:12:19 -0800

    added a piechart on projects, tidied style sheet

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 
 async function loadProjects() {
@@ -31,3 +32,19 @@ async function loadProjects() {
 
 // Load projects when the page loads
 loadProjects();
+
+// SVG Pie Chart code
+const svg = d3.select('#projects-pie-plot');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc,i) => {
+    svg.append('path').attr('d', arc).attr('fill', colors(i));
+  })

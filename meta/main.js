4851eec395c92add7401559b18d5a04aa5155862
nathansso <noliver@ucsd.edu>
2025-02-12 11:38:28 -0800

    Trigger github action

---
+++ meta/main.js
@@ -0,0 +1,11 @@
+let data = [];
+;
+
+async function loadData() {
+  data = await d3.csv('loc.csv');
+  console.log(data);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+});
